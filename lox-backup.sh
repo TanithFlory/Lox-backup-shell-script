7da290e49c2c38714276aa263be709821abc4e13
TanithFlory <tanithflory7770@gmail.com>
2024-02-10 13:32:39 +0000

    remove curl logs

--- lox-backup.sh
+++ lox-backup.sh
@@ -4,7 +4,6 @@ NAME="MODO_SHOWROOM"
 USER=$MS_USERNAME
 PASSWORD=$MS_PASSWORD
 IP="192.168.1.245"
-DATE="[$(date +'%a %b %d %T %Y')]"
 
 if [ ! -d "/miniserver-backups/$NAME" ]; then
 	mkdir "/miniserver-backups/$NAME"
@@ -13,15 +12,15 @@ fi
 
 cd "/miniserver-backups/$NAME"
 
-echo "${DATE} Loxone Miniserver Backup Initiated.\n"
+echo "[$(date +'%a %b %d %T %Y')] Loxone Miniserver Backup Initiated"
 
 wget --tries=2 -r -N -l inf -o wget.log ftp://$USER:$PASSWORD@$IP
 
 if [ $? -eq 0 ]; then
 
-	echo "${DATE} Backup Created Successfully, Location - $(pwd)"
+	echo "[$(date +'%a %b %d %T %Y')] Backup Created Successfully, Location - $(pwd)"
 
-	echo "Compressing The Backup."
+	echo "[$(date +'%a %b %d %T %Y')] Compressing The Backup."
 
 	FILE_NAME="${NAME}-$(date +'%m%d%Y').tar.gz"
 
@@ -29,9 +28,9 @@ if [ $? -eq 0 ]; then
 
 	rm -r "/miniserver-backups/${NAME}/$IP"
 
-	echo "${DATE} Compression Succesful."
+	echo "[$(date +'%a %b %d %T %Y')] Compression Succesful."
 
-	echo "${DATE} Initiating Upload To Google Drive"
+	echo "[$(date +'%a %b %d %T %Y')] Initiating Upload To Google Drive"
 
 	ACCESS_TOKEN=$(curl --location --request POST 'https://oauth2.googleapis.com/token' \
 		--header 'Content-Type: application/x-www-form-urlencoded' \
@@ -39,25 +38,25 @@ if [ $? -eq 0 ]; then
 		--data-urlencode "client_id=$CLIENT_ID" \
 		--data-urlencode "client_secret=$CLIENT_SECRET" \
 		--data-urlencode "refresh_token=$REFRESH_TOKEN" \
-		--data-urlencode "redirect_uri=$REDIRECT_URI" | jq -r '.access_token')
+		--data-urlencode "redirect_uri=$REDIRECT_URI" | jq -r '.access_token' >/dev/null 2>&1)
 
 	if [ $? -eq 0 ]; then
-		echo "${DATE} Access Token Granted From Google Drive, Uploading Backup \n"
+		echo "[$(date +'%a %b %d %T %Y')] Access Token Granted From Google Drive, Uploading Backup"
 
 		curl -X POST -L \
 			-H "Authorization: Bearer $ACCESS_TOKEN" \
-			-F "metadata={name:'Backup'};type=application/json;charset=UTF-8" \
+			-F "metadata={name:'${FILE_NAME}'};type=application/json;charset=UTF-9" \
 			-F "file=@/miniserver-backups/${NAME}/${FILE_NAME};type=application/x-tar" \
-			"https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+			"https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart" >/dev/null 2>&1
 
 		if [ $? -eq 0 ]; then
-			echo "${DATE} Upload Successful\n"
+			echo "[$(date +'%a %b %d %T %Y')] Upload Successful"
 		else
-			echo "${DATE} Upload Failed\n"
+			echo "[$(date +'%a %b %d %T %Y')] Upload Failed"
 		fi
 
 	else
-		echo "Failed to generate Token. \n"
+		echo "Failed to generate Token."
 	fi
 else
 
